a2af3968d914d9bc23cfe812aa94aa8c82ccca3b
Sazzad Hossain <sharifh530@gmail.com>
2020-07-27 12:41:27 +0600

    From disease.sh api we fetch the country and put it into a dropbox and made the header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
+//https://disease.sh/v3/covid-19/countries
+
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("Worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, //Bangladesh, India
+            value: country.countryInfo.iso2, //BAN , IND
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  };
 
   return (
     <div className="app">
+      {/* header */}
+      {/* Title+selec input dropdown field */}
+
       <div className="app_header">
+        <h1>COVID 19 TRACKER</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
             {/* Loop through all the countries and show a dropdown list of the options  */}
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
 
             {/* <MenuItem value="worldwide">Worldwide</MenuItem>
             <MenuItem value="worldwide">Worldwide</MenuItem>
@@ -20,14 +54,12 @@ function App() {
           </Select>
         </FormControl>
       </div>
-      <h1>COVID 19 TRACKER</h1>
 
-      {/* header */}
-      {/* Title+selec input dropdown field */}
-
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
+      <div className="app_starts">
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+      </div>
 
       {/* Table */}
       {/* Graph */}
